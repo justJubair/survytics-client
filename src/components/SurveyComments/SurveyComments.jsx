cad857c68e9b50eed6078271721675bd68ad594c
Jubair <jubair.ahmed7946@gmail.com>
2023-11-25 01:43:28 +0600

    Add: firebase auth added

---
+++ src/components/SurveyComments/SurveyComments.jsx
@@ -0,0 +1,7 @@
+const SurveyComments = () => {
+    return(
+        <div className="max-w-screen-lg mx-auto">
+             <p> HELLO I Am SurveyComments </p>
+        </div>
+    )}
+export default SurveyComments;
--- src/pages/SurveyDetail/SurveyDetail.jsx
+++ src/pages/SurveyDetail/SurveyDetail.jsx
@@ -1,33 +1,103 @@
 import { useQuery } from "@tanstack/react-query";
 import { useParams } from "react-router-dom";
 import axiosPublic from "../../api/axiosPublic";
-import surveyDetailImg from "../../assets/images/surveyDetail.png"
-
+import surveyDetailImg from "../../assets/images/surveyDetail.png";
+import {
+  FaRegThumbsDown,
+  FaRegThumbsUp,
+  FaThumbsDown,
+  FaThumbsUp,
+} from "react-icons/fa6";
+import SurveyComments from "../../components/SurveyComments/SurveyComments";
 const SurveyDetail = () => {
-    const {id} = useParams()
-    const {data:survey=[], refetch, isLoading} = useQuery({
-        queryKey: [id, "survey"],
-        queryFn: async()=>{
-            const res = await axiosPublic(`/survey/${id}`)
-            return res.data
-        }
-    })
-   
-    return(
-        <div className="max-w-screen-xl mx-auto">
-            <div className="h-[500px] flex items-center justify-center">
-            <div className=" flex items-center justify-center">
-                {/* text */}
-                <div className="md:w-1/3">
-                    <h2 className="lg:text-4xl">Shape the Future: Your Voice Matters! Join Our Online Survey Now</h2>
-                    <h3>Be Heard, Make a Difference: Dive into our Quick and Engaging Survey to Share Your Insights</h3>
-                </div>
-                {/* animation */}
-                <div>
-                    <img src={surveyDetailImg} alt="" />
-                </div>
-            </div>
-            </div>
+  const { id } = useParams();
+  const {
+    data: survey = [],
+    refetch,
+    isLoading,
+  } = useQuery({
+    queryKey: [id, "survey"],
+    queryFn: async () => {
+      const res = await axiosPublic(`/survey/${id}`);
+      return res.data;
+    },
+  });
+  const {
+    _id,
+    title,
+    question,
+    description,
+    category,
+    deadline,
+    like,
+    dislike,
+    VoteYes,
+    VoteNo,
+  } = survey;
+
+  return (
+    <div className="max-w-screen-xl mx-auto">
+      {/* banner */}
+      <div className="h-[400px] pt-28 flex items-center justify-center px-4 lg:px-0">
+        <div className=" flex items-center justify-center">
+          {/* text */}
+          <div className="w-full md:w-1/2">
+            <h2 className="text-2xl md:text-4xl font-semibold  text-green-600">
+              Survey title:{" "}
+              <span className="text-black leading-tight">{title}</span>
+            </h2>
+          </div>
+          {/* animation */}
+          <div>
+            <img className="" src={surveyDetailImg} alt="" />
+          </div>
         </div>
-    )}
+      </div>
+      {/* survey details */}
+      <div className="bg-base-300 max-w-screen-lg mx-auto p-10 rounded-lg shadow-xl space-y-3 my-10">
+        <p>
+          <span className="font-semibold">Descripton:</span> {description}
+        </p>
+        <p>
+          <span className="font-semibold">Deadline:</span> {deadline}
+        </p>
+        <p>
+          <span className="font-semibold">Category:</span> {category}
+        </p>
+        <p className="font-semibold">
+          <span className="font-semibold text-xl text-green-600">
+            Question:
+          </span>{" "}
+          {question}
+        </p>
+        {/* yes and no buttons */}
+        <div className="space-x-4">
+          <button className="btn bg-green-600 text-white hover:bg-white hover:text-green-600">
+            YES
+          </button>
+          <button className="btn bg-green-600 text-white hover:bg-white hover:text-green-600">
+            NO
+          </button>
+        </div>
+        {/* like and dislike button */}
+        <div className="flex justify-end items-center gap-4">
+          <p className="flex items-center text-lg gap-2">
+            {like}{" "}
+            <button>
+              <FaRegThumbsUp size={20} />
+            </button>
+          </p>
+          <p className="flex items-center text-lg gap-2">
+            {dislike}{" "}
+            <button>
+              <FaRegThumbsDown size={20} />
+            </button>
+          </p>
+        </div>
+      </div>
+      {/* comment section */}
+      <SurveyComments/>
+    </div>
+  );
+};
 export default SurveyDetail;
