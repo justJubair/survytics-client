3d8146a0fc8eea38b0fbf63aa2a21f0bc1294d25
Jubair <jubair.ahmed7946@gmail.com>
2023-11-26 20:20:03 +0600

    Add: stripe client secret fetched from backend

--- src/api/index.js
+++ src/api/index.js
@@ -57,3 +57,4 @@ export const patchLikeAndDislike = async(surveyId, operation)=>{
 }
 
 
+
--- src/pages/ProMember/CheckoutForm.jsx
+++ src/pages/ProMember/CheckoutForm.jsx
@@ -1,8 +1,49 @@
-import { CardElement } from "@stripe/react-stripe-js";
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import useAuth from "../../hooks/useAuth";
+import { useEffect, useState } from "react";
+import axiosSecure from "../../api/axiosSecure";
 
 const CheckoutForm = () => {
+  const stripe = useStripe()
+  const {user} = useAuth()
+  const elements = useElements()
+  const [clientSecret, setClientSecret] = useState("")
+  const [error, setError] = useState("")
+
+  useEffect(()=>{
+    axiosSecure.post("/create-payment-intent", {price: 25})
+    .then(res=>{
+      setClientSecret(res.data.clientSecret)
+    
+    })
+  },[])
+  console.log(clientSecret)
+
   const handleSubmit = async (e) => {
     e.preventDefault();
+
+    if(!stripe || !elements){
+      return; 
+    }
+
+    const card = elements.getElement(CardElement);
+    if(card===null){
+      return;
+    }
+
+    const {error, paymentMethod} = await stripe.createPaymentMethod({
+      type: "card",
+      card
+    })
+    if(error){
+      console.log(error)
+      setError(error.message)
+    } else {
+      setError("")
+      console.log(paymentMethod)
+    }
+
+
   };
   return (
     <div className="mt-24 max-w-2xl mx-auto px-4 md:mt-0">
@@ -28,10 +69,12 @@ const CheckoutForm = () => {
         <button
           className="btn w-full  bg-gradient-to-r from-cyan-600 to-[#24962a] hover:scale-95"
           type="submit"
+          disabled={!stripe}
         >
           Pay
         </button>
       </div>
+      <p className="text-red-600 font-semibold">{error}</p>
     </form>
     </div>
   );
